e0bc9aa09f09a35c659553c1e39b42c77279db19
Jakub Woźniak <j.wozniak@tenesys.pl>
2016-05-05 00:49:26 +0200

    license

--- dhcpd.go
+++ dhcpd.go
@@ -1,3 +1,21 @@
+/**
+    Simple DHCP Server
+    Copyright (C) 2016  Jakub Wozniak
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 package main
 
 import "fmt"
@@ -14,7 +32,7 @@ func main() {
   if uid != 0 {
     log.Fatal("You have to be root to run this server")
     os.Exit(1)
-  }  
+  }
 }
 
 
