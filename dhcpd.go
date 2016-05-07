819c5ec61857ec5e89d129962f810bd7a0702258
Jakub Woźniak <j.wozniak@tenesys.pl>
2016-05-07 14:28:30 +0200

    some sockets

--- dhcpd.go
+++ dhcpd.go
@@ -19,13 +19,13 @@
 package main
 
 import (
-    "fmt"
     "os"
     "log"
-    //"golang.org/x/sys/unix"
+    "golang.org/x/sys/unix"
     )
 
 
+// it will fail if uid is not 0
 func CheckPrivileges(uid int) {
   if uid != 0 {
     log.Fatal("You have to be root to run this server")
@@ -34,11 +34,46 @@ func CheckPrivileges(uid int) {
 }
 
 
+// Create UDP socket, bind it to device and set some options
+func BuildSocket(device string) int {
+  fd, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, 0)
+  if err != nil {
+    log.Fatal("Cannot create socket: %v", err)
+    os.Exit(1)
+  }
+
+  err = unix.BindToDevice(fd, device)
+  if err != nil {
+    log.Fatal("Cannot bind to device %v: %v", device, err)
+    os.Exit(1)
+  }
+
+  err = unix.SetsockoptInt(fd, unix.SO_REUSEADDR, 1)
+  if err != nil {
+    log.Fatal("Cannot set REUSEADDR: %v", err)
+    os.Exit(1)
+  }
+
+  err = unix.SetsockoptInt(fd, unix.SO_BROADCAST, 1)
+  if err != nil {
+    log.Fatal("Cannot set BROADCAST: %v", err)
+    os.Exit(1)
+  }
+
+
+  return fd
+}
+
+
+
 func main() {
   log.Print("Simple DHCP Server")
   log.Print("Copyright (C) 2016 Jakub Wozniak")
   log.Print("Distributed Systems, Poznan University of Technology")
 
   CheckPrivileges(os.Getuid())
-  fmt.Println(os.Getuid())
+  
+  // create socket
+  sfd := BuildSocket("wlan0")
+  log.Print(sfd)
 }
